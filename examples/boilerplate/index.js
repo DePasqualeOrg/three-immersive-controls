78d716dcd281cf701b30f366cf4ab0898d59d619
Anthony DePasquale <anthony@depasquale.org>
2022-03-23 10:30:45 +0100

    Clean up example

--- examples/boilerplate/index.js
+++ examples/boilerplate/index.js
@@ -6,10 +6,8 @@ const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.shadowMap.enabled = true;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
 
 const controls = new ImmersiveControls(camera, renderer, scene, {
   showFps: true,
